21341d3aac5bcf3c779f5d36a2c0b95d24e94aa3
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-08-30 20:07:01 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { ErrorComponent } from './components/error/error.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { UserprofileComponent } from './components/userprofile/userprofile.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, ErrorComponent, LandingComponent, UserprofileComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
