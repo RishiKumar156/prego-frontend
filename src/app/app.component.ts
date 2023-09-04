4073de5523ba24b7e48f85c5a27311796eeca72f
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-09-04 22:50:01 +0530

    google login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { EmailSignUpComponent } from './components/email-sign-up/email-sign-up.component';
 import { Component, HostListener } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,13 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = 'pregofrontend';
   isScrolled = false;
-
+  constructor(private dialog: MatDialog) {}
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 0;
+  }
+
+  login() {
+    this.dialog.open(EmailSignUpComponent, { panelClass: 'loginDialog' });
   }
 }
