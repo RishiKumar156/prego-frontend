e53390cff1e6443cbd31901f108399dbf6f420d1
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-09-10 22:26:58 +0530

    new updates

--- src/app/components/ordernow/ordernow.component.ts
+++ src/app/components/ordernow/ordernow.component.ts
@@ -1,15 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-ordernow',
   templateUrl: './ordernow.component.html',
-  styleUrls: ['./ordernow.component.scss']
+  styleUrls: ['./ordernow.component.scss'],
 })
 export class OrdernowComponent implements OnInit {
-
-  constructor() { }
+  foodData: any;
+  allFoods: string = 'OrderNow/Foods';
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get(`${environment.baseURL}/${this.allFoods}`)
+      .subscribe((foods: any) => {
+        this.foodData = foods;
+        console.log(foods);
+      });
   }
-
 }
